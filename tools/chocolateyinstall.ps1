d6602ce14827796147eb1521ffe7acda01af3295
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-08-12 11:49:49 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-major-win32.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'gcc-arm-embedded*'
+  checksum      = 'bc8ae26d7c429f30d583a605a4bcf9bc'
+  checksumType  = 'MD5'
+}
+Install-ChocolateyZipPackage @packageArgs
