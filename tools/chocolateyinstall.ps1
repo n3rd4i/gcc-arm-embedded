050e5ef6984459933d4584c622151443962cb748
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-03-12 11:12:55 +0100

    updated to 9.2.1 (9-2019q4) version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-win32.zip'
+$url        = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-win32.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'gcc-arm-embedded*'
-  checksum      = 'bc8ae26d7c429f30d583a605a4bcf9bc'
-  checksumType  = 'MD5'
+  checksum      = 'E4C964ADD8D0FDCC6B14F323E277A0946456082A84A1CC560DA265B357762B62'
+  checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
