36c8ca2336903e4aa9e287fb6872ba50dc12e248
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-02-23 17:02:40 +0100

    updated to version 10.2.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-win32.zip'
+$url        = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-win32.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = '49d6029ecd176deaa437a15b3404f54792079a39f3b23cb46381b0e6fbbe9070'
-  checksumType  = 'sha256'
+  checksum      = '5ee6542a2af847934177bc8fa1294c0d'
+  checksumType  = 'md5'
 }
 Install-ChocolateyZipPackage @packageArgs
